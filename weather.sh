75c9cd023405bb05d3e01acb67ee2955d2d88034
alpheratzz <alpheratz93@gmail.com>
2014-03-19 18:23:33 +0300

    Added removal of html file in cycle; added file parsing and printing result

--- weather.sh
+++ weather.sh
@@ -3,5 +3,9 @@
 
 while (true)
 do
+	#if [ -f $PAGEFILE ]; then rm $PAGEFILE; fi
 	wget -t 1 -O $PAGEFILE http://www.gismeteo.by/weather-minsk-4248/
+	grep -m 1 "value m_temp c" $PAGEFILE > tmp
+	echo "It's " $(sed 's/[^\-\+0-9]//g' temp) "°C in Minsk now"
+	rm tmp
 done
